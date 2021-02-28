d8beab89519b0c6f533e2ba4c41fa97d29fc5aee
apatap23 <77858823+apatap23@users.noreply.github.com>
2021-02-28 13:46:51 -0500

    Update iterators.py

--- iterators.py
+++ iterators.py
@@ -1,3 +1,5 @@
+from random import randint
+
 count = 0 
 l = [6,8,2,4,8,3,5,2]
 
@@ -9,3 +11,16 @@ print(count)
 for num in range(10):
     count += num 
 print(count)
+
+l1 = [randint(1,10) for num in range(13)]
+i = 0
+num_search = 2
+
+while i < len(l1):
+    if l1[i] == num_search:
+        print (f"{num_search} found at index {i}")
+    i+=1
+    
+
+
+ 
